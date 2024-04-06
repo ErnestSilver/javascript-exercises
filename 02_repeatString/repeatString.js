794c8ec39b5d4b72fcbb741f1ea553cb5e525b84
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-05 23:03:37 -0700

    Solve exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let word = string;
+  let new_word = word;
+  if (num > 0) {
+    for (let index = 1; index < num; index++) {
+      new_word += word;
+    }
+  } else if (num == 0) {
+    new_word = "";
+  } else {
+    new_word = "ERROR";
+  }
+  return new_word;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const characters = string.split("");
+  const backwards = characters.reverse();
+  let newString = "";
+  for (let i = 0; i < backwards.length; i++) {
+    newString += backwards[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
