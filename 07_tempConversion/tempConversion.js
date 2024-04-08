8675861019a0a3e103ebd0e4633a7e61a87ea1af
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-08 12:26:20 -0700

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let cels = (temp - 32) / 1.8;
+  let rounded = Math.round(cels * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahr = temp * 1.8 + 32;
+  let rounded = Math.round(fahr * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
