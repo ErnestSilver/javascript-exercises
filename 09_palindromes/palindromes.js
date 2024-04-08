6b5cd893ca93559fbf70b5347ec7a24682b23f72
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-08 14:46:51 -0700

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const allowed = "qwertyuiopasdfghjklzxcvbnm1234567890";
 
+  const word = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => allowed.includes(char))
+    .join("");
+
+  const reverseWord = word.split("").reverse().join("");
+
+  return word === reverseWord;
 };
 
 // Do not edit below this line
