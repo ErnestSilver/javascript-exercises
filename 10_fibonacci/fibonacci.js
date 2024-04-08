7aa82e05ec3068d8e447a38177fdd2b2abd19370
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-08 15:36:01 -0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const fibonacci = [1];
+  let number = Number(num);
+  if (number > 0) {
+    let first = 0;
+    for (let i = 0; i < number; i++) {
+      fibonacci.push(first + fibonacci[i]);
+      first = fibonacci[i];
+    }
+    return fibonacci[num - 1];
+  } else if (number === 0) {
+    return 0;
+  } else {
+    return "OOPS";
+  }
 };
-
+fibonacci(15);
 // Do not edit below this line
 module.exports = fibonacci;
