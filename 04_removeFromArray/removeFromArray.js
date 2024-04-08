98617deebeb42be73e8fd3f6f18279124ffcf9e8
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-07 20:32:08 -0700

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  //   const arg = [...arguments];
+  let newArray = [];
 
+  if (args.length > 1) {
+    newArray = arr;
+
+    for (let i = 0; i < args.length; i++) {
+      for (let x = 0; x < arr.length; x++) {
+        if (arr[x] === args[i]) {
+          newArray.splice(x, 1);
+        }
+      }
+    }
+    // for (let i = 0; i < arr.length; i++) {
+    //   for (let x = 0; x < args.length; x++) {
+    //     if (arr[i] !== args[x]) {
+    //       newArray.splice(i, 0, arr[i]);
+    //     }
+    //   }
+    // }
+  } else if (args.length == 1) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== args[0]) {
+        newArray.splice(i, 0, arr[i]);
+      }
+    }
+  }
+
+  return newArray;
 };
-
+console.log(removeFromArray([1, 2, 2, 3], 2));
 // Do not edit below this line
 module.exports = removeFromArray;
