a2d2ecd3ea44b4557ea884419352521729f38f50
Ernest Sribnyi <sribnyi.ernest2000@gmail.com>
2024-04-08 15:53:41 -0700

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  return obj.map(getTitle);
+  function getTitle(item) {
+    return item.title;
+  }
 };
 
 // Do not edit below this line
